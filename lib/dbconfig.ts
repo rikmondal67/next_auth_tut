c096e48ac2e2ff778aa7842930cf66729b02d018
rikmondal67 <mondalrik67@gmail.com>
2026-05-09 01:39:32 +0530

    use mongoose old connection

--- lib/dbconfig.ts
+++ lib/dbconfig.ts
@@ -2,6 +2,10 @@ import mongoose  from "mongoose";
 
 async function connectdb(){
     const dburl:string=(process.env.MONGODB_URI)||'';
+    if(mongoose.connection.readyState >= 1){
+        console.log('db connection already exsist');
+        return;
+    }
     try{
         console.log(dburl);
         mongoose.connect(dburl);
